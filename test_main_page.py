ccb0da4a28fe22c398d558897aedb26051e439e6
o-master-o <kasian.m.w@gmail.com>
2024-03-14 21:22:25 +0100

    add test_check_entry_page_headers_consistence and test_check_sections_links

--- test_main_page.py
+++ test_main_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 from controller import ChromeDriver
-from data import expected_sections_list
+from data import expected_sections_data
 
 ENTRY_POINT = 'https://the-internet.herokuapp.com/'
 
@@ -15,25 +15,32 @@ def chrome():
     try:
         yield driver
     finally:
-        driver.close()
+        driver.quit()
 
 
 @pytest.fixture(scope='module')
 def content(chrome):
     chrome.get(ENTRY_POINT)
+    WebDriverWait(chrome, timeout=10).until(lambda d: d.execute_script("return document.readyState") == "complete")
     return chrome
 
 
-def test_check_entry_page_content(content):
+@pytest.fixture(scope='module')
+def sections(content):
+    sections_path = '/html/body/div[2]/div/ul'
+    return content.find_element(By.XPATH, sections_path)
+
+
+def test_check_entry_page_headers_consistence(content):
     welcome_header_path = '/html/body/div[2]/div/h1'
     sub_header_path = '/html/body/div[2]/div/h2'
-    sections_path = '/html/body/div[2]/div/ul'
-    header1 = WebDriverWait(content, timeout=10).until(EC.visibility_of_element_located((By.XPATH, welcome_header_path)))
+    header1 = content.find_element(By.XPATH, welcome_header_path)
     header2 = content.find_element(By.XPATH, sub_header_path)
-    sections_list = content.find_element(By.XPATH, sections_path)
-    sections = sections_list.find_elements(By.TAG_NAME, 'li')
     assert "Welcome to the-internet" == header1.text
     assert "Available Examples" == header2.text
-    assert expected_sections_list == [element.text for element in sections]
-#
 
+
+def test_check_sections_links(sections):
+    items = sections.find_elements(By.TAG_NAME, 'li')
+    sections_data = [(element.text, element.find_element(By.TAG_NAME, 'a').get_attribute("href")) for element in items]
+    assert expected_sections_data == sections_data
